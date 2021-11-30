1ce493275f0f7c3ef1b07c4cc0020e9da2368eae
Ali Asgar <ali.asgar2019@vitbhopal.ac.in>
2021-11-30 12:59:23 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,34 @@
 // main function of Secode
+header.h
+intro.cpp
+admin_menu.cpp
+    
+
+void main()
+{
+    char ch;
+    intro();
+    do
+    {
+        clrscr();
+        cout<<"\n\n\n\tMAIN MENU";
+        cout<<"\n\n\t01. BOOK ISSUE";
+        cout<<"\n\n\t02. BOOK DEPOSIT";
+          cout<<"\n\n\t03. ADMINISTRATOR MENU";
+          cout<<"\n\n\t04. EXIT";
+          cout<<"\n\n\tPlease Select Your Option (1-4) ";
+          ch=getche();
+          switch(ch)
+          {
+            case '1':clrscr();
+                 book_issue();
+                    break;
+              case '2':book_deposit();
+                     break;
+              case '3':admin_menu();
+                 break;
+              case '4':exit(0);
+              default :cout<<"\a";
+        }
+        }while(ch!='4');
+}

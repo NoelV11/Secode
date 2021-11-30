5fa5c0eba379402a5241291590719932e72ca3cf
Ali Asgar <ali.asgar2019@vitbhopal.ac.in>
2021-11-30 13:25:35 +0530

    Update IntroSreen.cpp

--- IntroSreen.cpp
+++ IntroSreen.cpp
@@ -1,12 +1,20 @@
 int intro()
 {
     clrscr();
+    
+    //  Goto lcation 35, 11  
     gotoxy(35,11);
-    cout<<"LIBRARY";
+    cout<<"Secode: Insecure Code Detector";
+    
+    //  Goto lcation 35, 14 
     gotoxy(35,14);
-    cout<<"MANAGEMENT";
+    cout<<"Made By Ali Asgar, oorja Rungat, Shirshak Kumar and Noel Varghese";
+    
+    //  Goto lcation 35, 17 
     gotoxy(35,17);
-    cout<<"SYSTEM";
-    cout<<"\n\nMADE BY : ALI ASGAR";
+    cout<<"VIT'B Project Exhibition - 2";
+    
+    
+    cout<<"\n\n Supervised By: Sir Thangavel M";
     getch();
 }

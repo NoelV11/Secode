cb3eee3fc2d0c7a7462152e3751d479d11ee1e47
Ali Asgar <ali.asgar2019@vitbhopal.ac.in>
2021-11-30 13:11:03 +0530

    App Intro

---
+++ IntroSreen.cpp
@@ -0,0 +1,12 @@
+int intro()
+{
+    clrscr();
+    gotoxy(35,11);
+    cout<<"LIBRARY";
+    gotoxy(35,14);
+    cout<<"MANAGEMENT";
+    gotoxy(35,17);
+    cout<<"SYSTEM";
+    cout<<"\n\nMADE BY : ALI ASGAR";
+    getch();
+}

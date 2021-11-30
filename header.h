538f2432b2b84da702c4bb56deac3060d6588237
Ali Asgar <ali.asgar2019@vitbhopal.ac.in>
2021-11-30 16:11:36 +0530

    App Header

--- header.h
+++ header.h
@@ -1,1 +1,22 @@
 // Header Files For The Secode "Main Function"
+
+#include<iostream.h>
+#include<conio.h>
+
+// a header file which has the necessary information to include the input/output related functions in our program
+#include<stdio.h>
+
+/* C header file which contains function declarations and macros used in working with threads and processes.
+Here, used for "getoxy function"
+*/
+#include<process.h>
+
+// String operations
+#include<string.h>
+
+/*
+  The header <iomanip> is part of the Input/output library of the C++ Standard Library. 
+  It defines the manipulator functions resetiosflags(), setiosflags(), setbase(), 
+  setfill(), setprecision(), and setw(). These functions may be conveniently used by C++ programs to affect the state of iostream objects.
+*/
+#include<iomanip.h>

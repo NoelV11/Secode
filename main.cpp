7091410bcbe62a2533b32599389dda14468e9a6c
Ali Asgar <ali.asgar2019@vitbhopal.ac.in>
2021-11-30 13:14:12 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,27 +1,40 @@
 // main function of Secode
+
+// Calling Files
 header.h
-admin_menu.cpp
+IntroSreen.cpp 
+Options.cpp
     
 
 int main()
 {
     char ch;
+    
+    // Invoking IntroSreen.cpp 
     intro();
     do
-    {
-        clrscr();
-        cout<<"\n\n\n\tMAIN MENU";
-        cout<<"\n\n\t01. C Insecure Functions";
-          cout<<"\n\n\t02. EXIT";
-          cout<<"\n\n\tPlease Select Your Option (1-2) ";
-          ch=getche();
-          switch(ch)
-          {
-            case '1':clrscr();
-                 C_ruleset();
-                    break;
-              case '2':exit(0);
-              default: cout<<"Wrong Option";
-        }
-        }while(ch!='2');
+        {
+            clrscr();
+
+            // Options.cpp
+            cout<<"\n\n\n\t Options";
+            cout<<"\n\n\t   01. C Insecure Functions";
+              cout<<"\n\n\t 02. EXIT";
+              cout<<"\n\n\t Please Select Your Option (1-2) ";
+
+            // getting Options
+              ch=getche();
+
+            // Using Switch Case
+              switch(ch)
+              {
+                case '1':clrscr();
+                     C_ruleset();
+                        break;
+                  case '2':exit(0);
+                  default: cout<<"Wrong Option";
+            }
+
+            }
+    while(ch!='2');
 }

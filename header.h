6250ea54a147318de3d68e7095833bd58e7661ab
Ali Asgar Abid Sabunwala <ali.asgar2019@vitbhopal.ac.in>
2021-12-04 19:58:19 +0530

    Update header.h

--- header.h
+++ header.h
@@ -1,15 +1,25 @@
 // Header Files For The Secode "Main Function"
 
+// operation's on File
+#include<fstream.h>
+
+// A header file which includes input/ output
 #include<iostream.h>
+
+// A header file which includes inbuilt functions like getch() and clrscr().It stand for console input ouput i.e. it takes input from keyboard and displays it on screen.
+// The gotoxy() funtion is used to simply move the cursor on your monitor screen wherever desired. 
+// It is declared in the "conio. h" header file. It takes 2 parameters, gotoxy(int x, int y).
 #include<conio.h>
 
-// a header file which has the necessary information to include the input/output related functions in our program
+// A header file which has the necessary information to include the input/output related functions in our program
 #include<stdio.h>
 
 /* C header file which contains function declarations and macros used in working with threads and processes.
-Here, used for "getoxy function"
-*/
+Here, used for "gotoxy function"
+The gotoxy function is used to to move the cursor to a specified location on the screen. Its Just Something Other then just Sequential Execution. 
+It moves the cursor with respect to x axis and y axis. It is very useful when the output is to be displayed at a specified location on the screen.
 #include<process.h>
+*/
 
 // String operations
 #include<string.h>
@@ -20,3 +30,4 @@ Here, used for "getoxy function"
   setfill(), setprecision(), and setw(). These functions may be conveniently used by C++ programs to affect the state of iostream objects.
 */
 #include<iomanip.h>
+

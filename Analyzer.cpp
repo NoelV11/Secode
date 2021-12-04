84653e16db54689315db565e1d48199f24d6aceb
Ali Asgar Abid Sabunwala <ali.asgar2019@vitbhopal.ac.in>
2021-12-04 20:20:57 +0530

    Analyzer

--- Analyzer.cpp
+++ Analyzer.cpp
@@ -1,11 +1,36 @@
-// get file 
+int Analyzer()
+{
+  // Get And open File
+  // Here, performance is critical for our software, thus we consider using the C language. This code is 4-5 times faster than the C++ versions
+  FILE* fp = fopen(FILENAME, "r");
+if (fp == NULL)
+    exit(EXIT_FAILURE);
 
-// open file 
+char* line = NULL;
+size_t len = 0;
+while ((getline(&line, &len, fp)) != -1) {
+    // using printf() in all tests for consistency
+    printf("%s", line);
+}
+fclose(fp);
+if (line)
+    free(line);
+   
 
 // scan if .c file 
-
+  // Here, R_OK, W_OK, and X_OK to check for read permission, write permission, and execute permission respectively.
+if( access( fname, R_OK | W_OK | X_OK ) == 0 ) {
+    // file exists in C
+} else {
+    // file doesn't exist in C
+}
+  
 // Scan header's
 
 // ignore Comment's
 
 // Lexical Analyzer 
+  ruleset();
+  
+  // Conditional Statement's
+}

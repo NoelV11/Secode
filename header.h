a6dab777173fc419ac55aca34e598650cdbe3adb
Ali Asgar Abid Sabunwala <ali.asgar2019@vitbhopal.ac.in>
2021-12-06 21:05:46 +0530

    header

--- header.h
+++ header.h
@@ -14,23 +14,8 @@
 // A header file which has the necessary information to include the input/output related functions in our program
 #include<stdio.h>
 
-/* C header file which contains function declarations and macros used in working with threads and processes.
-Here, used for "gotoxy function"
-The gotoxy function is used to to move the cursor to a specified location on the screen. Its Just Something Other then just Sequential Execution. 
-It moves the cursor with respect to x axis and y axis. It is very useful when the output is to be displayed at a specified location on the screen.
-#include<process.h>
-*/
-
 // String operations
 #include<string.h>
-
-/*
-  The header <iomanip> is part of the Input/output library of the C++ Standard Library. 
-  It defines the manipulator functions resetiosflags(), setiosflags(), setbase(), 
-  setfill(), setprecision(), and setw(). These functions may be conveniently used by C++ programs to affect the state of iostream objects.
-
-#include<iomanip.h>
-*/
 
 // access() function, found in unistd.h used in Analyzer
 #include<unistd.h>

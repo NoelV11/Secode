7751369030ec958878dbf13b3cb15e2b3c435ae6
Ali Asgar Abid Sabunwala <ali.asgar2019@vitbhopal.ac.in>
2021-12-06 18:54:38 +0530

    Update SplashSreen.cpp

--- SplashSreen.cpp
+++ SplashSreen.cpp
@@ -2,25 +2,23 @@ int SplashScreen()
 {
     // Cleaning up the screen and memory to avoid any memory leakage or anything like that
     clrscr();
-    
-    //  Goto lcation 35, 11  
-    gotoxy(35,11);
+
+    int x, y;     // // gotoxy initialization 
+    gotoxy(x, y); // gotoxy definition 
+
+    //  Goto lcation 20, 11
+    gotoxy(20, 11);
     cout<<"Secode: Insecure Code Detector";
     
-    //  Goto lcation 35, 14 
-    gotoxy(35,14);
+    //  Goto lcation 10, 14
+    gotoxy(10, 14);
     cout<<"Made By Ali Asgar, oorja Rungat, Shirshak Kumar and Noel Varghese";
     
-    //  Goto lcation 35, 17 
-    gotoxy(35,17);
+    //  Goto lcation 20, 17 
+    gotoxy(20,17);
     cout<<"VIT'B Project Exhibition - 2";
     
-     //  Goto lcation 35, 19
-    gotoxy(35,19);
+     //  Goto lcation 25, 19
+    gotoxy(25,19);
     cout<<"\n\n Supervised By: Sir Thangavel M";
-    
-    
-    cout<<"\n\n Press any key to continue";
-    
-    getch();
 }

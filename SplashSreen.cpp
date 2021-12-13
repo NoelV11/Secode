7d3644bb93ff8219b2d826ef84c39c07191d1881
Ali Asgar Abid Sabunwala <ali.asgar2019@vitbhopal.ac.in>
2021-12-13 20:41:17 +0530

    Splashscreen

--- SplashSreen.cpp
+++ SplashSreen.cpp
@@ -8,17 +8,17 @@ int SplashScreen()
 
     //  Goto lcation 20, 11
     gotoxy(20, 11);
-    cout<<"Secode: Insecure Code Detector";
+    cout<< "Secode: Insecure Code Detector";
     
     //  Goto lcation 10, 14
     gotoxy(10, 14);
-    cout<<"Made By Ali Asgar, oorja Rungat, Shirshak Kumar and Noel Varghese";
+    cout<< "Made By Ali Asgar, Oorja Rungta, Shirshak Kumar and Noel Varghese";
     
     //  Goto lcation 20, 17 
     gotoxy(20,17);
-    cout<<"VIT'B Project Exhibition - 2";
+    cout<< "VIT'B Project Exhibition - 2";
     
      //  Goto lcation 25, 19
     gotoxy(25,19);
-    cout<<"\n\n Supervised By: Sir Thangavel M";
+    cout<< "Supervised By: Sir Thangavel M";
 }
